141cc03b43fcf0906a78d93c72986cbc5d837db0
Hemanth <hemanthkumar.s@gmail.com>
2019-04-11 09:41:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import numpy as np
+from scipy import optimize
+
+from rayleigh import cerate_A_sym_mat
+from rayleigh import RayleighQuotient
+
+# Set Seed Value
+# np.random.seed(3)
+
+# First Eigen Vector
+dim = 4
+A,V,L = cerate_A_sym_mat(dim)
+
+# To handle Rank defeciency
+c = 10
+A = A + c*np.eye(dim)
+
+rq_obj = RayleighQuotient(A)
+
+x0 = np.random.randn(dim)
+x0 = x0/np.linalg.norm(x0)
+result = optimize.minimize(rq_obj.eval, x0, method='Newton-CG', jac=rq_obj.grad, hess=rq_obj.hessian, options={'xtol': 1e-3, 'disp': False})  
+V0 = result.x/np.linalg.norm(result.x)
+l0 = result.fun
+
+
+rq_obj.update_func(V0.reshape((dim,1)))
+x0 = np.random.randn(dim)
+x0 = x0/np.linalg.norm(x0)
+result1 = optimize.minimize(rq_obj.eval, x0, method='Newton-CG', jac=rq_obj.grad, hess=rq_obj.hessian, options={'xtol': 1e-3, 'disp': False})  
+V1 = result1.x/np.linalg.norm(result1.x)
+V1 = V1 - np.dot(V1,V0)*V0
+V1 = V1/np.linalg.norm(V1)
+l1 = result1.fun
+
+print('lambda_0 = {}, lambda_0 = {}'.format(np.sign(l0)*l0 - c,np.sign(l1)*l1 - c))
+
---
+++ rayleigh.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+from scipy import optimize
+import pdb
+
+def cerate_A_sym_mat(dim=3):
+	Q,_ = np.linalg.qr(np.random.randn(dim,dim) *np.random.randn(dim,dim))
+	s = np.random.choice(dim) + 1
+	S = np.diag(list(reversed(range(s,s+dim))))**2
+	return Q.dot(S).dot(Q.T), Q, S
+
+class RayleighQuotient():
+	def __init__(self, A):
+		if not self.check_symmetric(A):
+			print('Matrix must be symmetric!!')
+			sys.exit()
+
+		self.dim = A.shape[0]
+		self.A = A
+		self.X = self.create_var_X(self.dim)
+		self.f = self.func()
+		self.df = self.f.diff(self.X)
+		self.H = self.df.diff(self.X)
+
+	def check_symmetric(self, A, tol=1e-8):
+		    return np.allclose(A, A.T, atol=tol)
+
+	def create_var_X(self, dim=3):
+		X = ['x'+str(i) for i in range(dim)]
+		return np.array(sp.symbols(X)).T
+
+	def func(self, V=None):
+		if V is None:
+			return -1*self.X.T.dot(self.A).dot(self.X)/self.X.T.dot(self.X)
+
+		Y = self.X
+		for i in range(V.shape[1]):
+			v = V[:,i].reshape(self.dim)
+			Y = Y - np.dot(self.X,v)*v
+		
+		return -1*Y.T.dot(self.A).dot(Y)/Y.T.dot(Y)	
+
+	def update_func(self, V):
+		self.f = self.func(V)
+		self.df = self.f.diff(self.X)
+		self.H = self.df.diff(self.X)
+		return
+
+	def eval(self, x):
+		value = {}
+		for var,val in zip(range(len(x)),x):
+			value['x'+str(var)] = val
+		return self.f.subs(value)
+
+	def grad(self, x):
+		value = {}
+		for var,val in zip(range(len(x)),x):
+			value['x'+str(var)] = val
+		return np.array(self.df.subs(value)).astype('float')
+
+	def hessian(self, x):
+		d = len(x)
+		value = {}
+		for var,val in zip(range(len(x)),x):
+			value['x'+str(var)] = val
+		return np.array(self.H.subs(value)).astype('float').reshape(d,d)
